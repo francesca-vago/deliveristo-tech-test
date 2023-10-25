f083b580b0f1d726f72edf6021ac4ac445ad474e
Francesca Vago <vago.francesca@gmail.com>
2023-10-25 11:42:16 +0200

    fix tests types with react testing library

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,8 +1,10 @@
-import { afterEach } from "vitest";
+import { afterEach, expect } from "vitest";
 import { cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/vitest";
+import * as matchers from "@testing-library/jest-dom/matchers";
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
 });
+
+expect.extend(matchers);
